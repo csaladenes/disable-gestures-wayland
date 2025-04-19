d39f0e73b2a9e9a543e363404af0dd26b92723d9
Dénes Csala <mail@csaladen.es>
2025-04-19 22:53:29 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# disable-gestures-wayland
-GNOME extension that completely disables all touchpad gestures on Wayland - YES, including the 3-finger swipes!
+# Disable Gestures Wayland
+
+A GNOME Shell extension that completely disables all touchpad gestures on Wayland.
+
+## Features
+
+- Disables all touchpad gestures (swipes, pinches, etc.)
+- Works specifically on Wayland - and it disables the newly introduced 3-finger swipe gestures!
+- Preserves keyboard shortcuts and mouse functionality
+
+## Installation
+
+### From extensions.gnome.org
+
+1. Visit [extensions.gnome.org](https://extensions.gnome.org)
+2. Search for "Disable Gestures Wayland"
+3. Click the install button
+
+### Manual Installation
+
+1. Download the extension zip file
+2. Extract it to `~/.local/share/gnome-shell/extensions/disable-wayland-gestures@zonetra/`
+3. Restart GNOME Shell (Alt+F2, type 'r' and press Enter) - or Log in and out
+4. Enable the extension in GNOME Extensions at `https://extensions.gnome.org/local/`
+
+## Usage
+
+Once enabled, all touchpad gestures will be disabled. The Windows/Super key and other keyboard shortcuts will continue to work normally.
+
+## Requirements
+
+- GNOME Shell 42, 43, or 44
+- Wayland display server
+
+## License
+
+GPL-2.0-or-later 
---
+++ extension.js
@@ -0,0 +1,63 @@
+/* extension.js
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+/* exported init */
+
+const Main = imports.ui.main;
+
+class Extension {
+  constructor() {}
+
+  enable() {
+    this._swipeMods = [
+      Main.overview._swipeTracker._touchpadGesture,
+      Main.wm._workspaceAnimation._swipeTracker._touchpadGesture,
+      Main.overview._overview._controls._workspacesDisplay._swipeTracker._touchpadGesture,
+    ];
+
+    this._swipeMods.forEach((g) => {
+      g._newHandleEvent = (actor, event) => {
+        // Block all gestures by returning false
+        return false;
+      };
+
+      global.stage.disconnectObject(g);
+      global.stage.connectObject(
+        'captured-event::touchpad',
+        g._newHandleEvent.bind(g),
+        g
+      );
+    });
+  }
+
+  disable() {
+    this._swipeMods.forEach((g) => {
+      global.stage.disconnectObject(g);
+      global.stage.connectObject(
+        'captured-event::touchpad',
+        g._handleEvent.bind(g),
+        g
+      );
+    });
+    this._swipeMods = [];
+  }
+}
+
+function init() {
+  return new Extension();
+} 
